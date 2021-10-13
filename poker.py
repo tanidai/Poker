d18b880f6c8602d47dcd93586ac2146eeac9ee01
tanidai <92280000+tanidai@users.noreply.github.com>
2021-10-13 15:37:37 +0900

    k

---
+++ poker.py
@@ -0,0 +1,27 @@
+import random
+def toranpu_():
+    torannpu = []
+    suuti = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    ma_ku = ["♡","♤","♧","♢"]
+    for i in suuti:
+        for j in ma_ku:
+            torannpu.append(i+j)
+    return torannpu
+def haifu(torannpu,fs_list,count=5):
+    fs_list = []
+    for i in range(count):
+        fs = random.choice(torannpu)
+        fs_list.append(fs)
+        torannpu.remove(fs)
+    return fs_list
+
+
+torannpu = toranpu_()
+pl_list = []
+dl_list = []
+
+pl = haifu(torannpu,pl_list)
+dl = haifu(torannpu,dl_list)
+print(pl)
+ko = input("このうち捨てるカードを選択してください*右から(0,1,2,3,4)*カンマ区切りで入力 >")
+print(torannpu)
