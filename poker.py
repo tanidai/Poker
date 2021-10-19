82a9907cb0cdf3f19608b872d75ca03a1dabfbf0
tanidai <92280000+tanidai@users.noreply.github.com>
2021-10-19 12:25:42 +0900

    l

--- poker.py
+++ poker.py
@@ -13,14 +13,23 @@ def haifu(torannpu,fs_list,count=5):
         fs_list.append(fs)
         torannpu.remove(fs)
     return fs_list
-
-
+def dlira(dl):
+    hogolist = []
+    for i in dl:
+        for l in dl:
+            if i[0] == l[0] and i != l:
+                hogolist.append(i)
+                hogolist.append(l)
+    count = 5 - len(hogolist)
+    dl_deta = haifu(torannpu,hogolist,count)
+    return dl_deta
 torannpu = toranpu_()
 pl_list = []
 dl_list = []
 pl = haifu(torannpu,pl_list)
 dl = haifu(torannpu,dl_list)
 print(pl)
+print(dl)
 ko = list(map(int,input("このうち捨てるカードを選択してください*右から(0,1,2,3,4)*カンマ区切りで入力、ない場合は５を入力 >").split(",")))
 ko = ko[::-1]
 if ko != [5]:
@@ -32,3 +41,4 @@ if ko != [5]:
     print(pl_2)
 elif ko == [5]:
     print(pl)
+print(dlira(dl))
